eecadb149dca5aa4532967aee76ca61addc241b9
Gianluca Maggiorotto <maggii0@MacBook-Pro-de-Xinyi.local>
2026-06-18 23:38:19 +0200

    fix 05 ex01 06

--- C_Piscine_C_06/ex01/ft_print_params.c
+++ C_Piscine_C_06/ex01/ft_print_params.c
@@ -6,20 +6,10 @@
 /*   By: gmaggior <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 18:18:37 by gmaggior          #+#    #+#             */
-/*   Updated: 2026/06/18 18:38:15 by gmaggior         ###   ########.fr       */
+/*   Updated: 2026/06/18 23:26:25 by gmaggior         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return(i);
-}
 
 int	main(int argc, char *argv[])
 {
@@ -29,16 +19,27 @@ int	main(int argc, char *argv[])
 	i = 1;
 	j = 0;
 	if (argc <= 1)
-		return(0);
+	{
+		write(1, "Insuficient parameters", 22);
+		return (0);
+	}
 	else
 	{
-		while (ft_strlen(&argv[i][j]))
+		while (i <= argc - 1)
 		{
 			write(1, &argv[i][j], 1);
 			j++;
-			if (&argv[i][j] == '\0')
+			if (argv[i][j] == '\0')
+			{
+				write(1, "\n", 1);
+				j = 0;
 				i++;
+			}
 		}
 	}
-	return (0);	
+	return (0);
 }
+/*
+Controlamos hasta argc - 1 porque argc final es valor NULL 
+entonces nos da segmentation fault
+*/
